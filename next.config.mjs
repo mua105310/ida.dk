6e6ce92cdfc2032e4bc98eb9ded83f1b0364c6e7
Thor <43585332+mua105310@users.noreply.github.com>
2025-05-10 22:51:33 +0200

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://moveboks.dk/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
